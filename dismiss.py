83bea3d8771c094009fdcd3a9a8af378ab670774
Jeff Lai <jefflaichunyin@gmail.com>
2019-04-22 21:44:01 +0800

    change web style

--- dismiss.py
+++ dismiss.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 import cgi
+import requests
 f=open("status","w")
 f.write("No")
 f.close()
-print('Content-type: text/html\n\n')
-print('<head><meta http-equiv="refresh" content="0; url=index.html" /></head>')
+
+drone_IP = "192.168.0.112"
+url = "http://" + drone_IP + "/off"
+try:
+    requests.get(url,timeout=1)
+except:
+    print('Content-type: text/html\n\n')
+    print('<head><meta http-equiv="refresh" content="0; url=index.html" /></head>')
---
+++ sendDrones.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import cgi
+import requests
+drone_IP = "192.168.0.112"
+url = "http://" + drone_IP + "/on"
+
+try:
+    requests.get(url,timeout=1)
+except:
+    print('Content-type: text/html\n\n')
+    print('<head><meta http-equiv="refresh" content="0; url=livestream.html" /></head>')
