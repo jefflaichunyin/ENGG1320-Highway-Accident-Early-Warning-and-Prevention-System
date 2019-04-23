aa95fdd841f02f552de786ccd737a9ad5b9d3b52
Jeff Lai <jefflaichunyin@gmail.com>
2019-04-23 23:12:53 +0800

    use pin A15 to control buzzer and LED

--- drone_control/wifi-control/wifi-control.ino
+++ drone_control/wifi-control/wifi-control.ino
@@ -4,8 +4,8 @@ char PASSWD[]="0434043402320232";
 int state=0;
 void connect()
 {
-  Serial1.println("AT+RST");
-  delay(500);
+  //Serial1.println("AT+RST");
+  //delay(3000);
   Serial1.println("AT+CWMODE=1");
   delay(500);
   Serial1.print("AT+CWJAP=\"");
@@ -13,7 +13,7 @@ void connect()
   Serial1.print("\",\"");
   Serial1.print(PASSWD);
   Serial1.println("\"");
-  delay(6000);
+  delay(10000);
   Serial1.println("AT+CIFSR");
   delay(500);
   Serial1.println("AT+CIPMUX=1");
@@ -29,7 +29,9 @@ void setup()
   while(!Serial1)
   {}
   pinMode(PC13,OUTPUT);
+  pinMode(PA15, OUTPUT);
   digitalWrite(PC13,HIGH);
+  digitalWrite(PA15,LOW);
   delay(1000);
   connect();
 
@@ -37,17 +39,17 @@ void setup()
 void changeState()
 {
   state = !state;
-  digitalWrite(PC13,state);
+  digitalWrite(PA15, state);
 }
 void ON()
 {
   Timer1.setMode(TIMER_CH1, TIMER_OUTPUTCOMPARE);
-  Timer1.setPeriod(1000000);
+  Timer1.setPeriod(500000);
   Timer1.attachInterrupt(TIMER_CH1, changeState);
 }
 void OFF()
 {
-  digitalWrite(PC13,HIGH);
+  digitalWrite(PA15,LOW);
   Timer1.detachInterrupt(TIMER_CH1);
 }
 void loop()
