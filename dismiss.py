d37de260b0806b88aa4fa7b56828472b243b1fbd
Jeff Lai <jefflaichunyin@gmail.com>
2019-04-25 14:06:14 +0000

    run/stop blinking with sudo

--- dismiss.py
+++ dismiss.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import cgi
 import requests
+import os
 f=open("status","w")
 f.write("No")
 f.close()
 
 drone_IP = "192.168.1.102"
 url = "http://" + drone_IP + "/off"
+os.system("echo 'engg1320' | sudo -S killall -9 blink.py")
+os.system("echo 'engg1320' | sudo -S ./stop_blinking.py")
 try:
     requests.get(url,timeout=1)
 except:
--- sendDrones.py
+++ sendDrones.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import cgi
 import requests
+import os
 drone_IP = "192.168.1.102"
 url = "http://" + drone_IP + "/on"
-
+os.system("echo 'engg1320' | sudo -S  ./blink.py")
 try:
     requests.get(url,timeout=1)
 except:
